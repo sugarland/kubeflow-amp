1a8e6b3c96cf5dc895df79a71828f85fbcc9cbd0
EC2 Default User <ec2-user@ip-172-31-16-133.us-east-2.compute.internal>
2022-02-25 20:38:27 +0000

    added eks setup

---
+++ eks_setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+CLUSTER_NAME=KUBEFLOW-EKS-CLUSTER-2
+
+# Install Kubectl
+
+sudo curl --silent --location -o /usr/local/bin/kubectl \
+   https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+
+sudo chmod +x /usr/local/bin/kubectl
+
+# Update AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
+# Utilities
+sudo yum -y install jq gettext bash-completion moreutils
+
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+kubectl completion bash >>  ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+# eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv -v /tmp/eksctl /usr/local/bin
+
+eksctl completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+# Create new cluster
+eksctl create cluster -f eks-create-cluster.yaml
+
+# Update context
+aws eks update-kubeconfig --name $CLUSTER_NAME
